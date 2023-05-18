48e11c82ff8c6fc1e16cc8faf4526393762fdbb2
Wjyeop <gpgp08@naver.com>
2023-05-18 11:29:09 +0900

    바드와 함께 하는 밸런스 게임

--- src/App.js
+++ src/App.js
@@ -1,25 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Board = styled.div`
+ background-color: #f0f8ff;
+ padding: 20px;
+ border: 1px solid #ccc;
+
+ /* 반응형 */
+
+ @media (max-width: 768px) {
+  width: 100%;
+  margin: 0 20px;
+ }
+`;
+
+const LeftColumn = styled.div`
+ float: left;
+ width: 50%;
+
+ /* 반응형 */
+
+ @media (max-width: 768px) {
+  width: 100%;
+  margin-bottom: 20px;
+ }
+`;
+
+const RightColumn = styled.div`
+ float: right;
+ width: 50%;
+
+ /* 반응형 */
+
+ @media (max-width: 768px) {
+  width: 100%;
+  margin-bottom: 20px;
+ }
+`;
+
+const Heading = styled.h1`
+ font-size: 18px;
+ font-weight: bold;
+ color: #333;
+
+ /* 반응형 */
+
+ @media (max-width: 768px) {
+  font-size: 16px;
+ }
+`;
+
+const Paragraph = styled.p`
+ font-size: 16px;
+ line-height: 1.5;
+ color: #666;
+
+ /* 반응형 */
+
+ @media (max-width: 768px) {
+  font-size: 14px;
+ }
+`;
+
+const TopBar = styled.div`
+ background-color: #337ab7;
+ color: #fff;
+ padding: 20px;
+
+ /* 반응형 */
+
+ @media (max-width: 768px) {
+  height: 40px;
+  padding: 0;
+ }
+`;
+
+const TopBarTitle = styled.h1`
+ font-size: 18px;
+ font-weight: bold;
+`
+
+const Image = styled.img`
+ width: 100%;
+ height: 100%;
+`
+
+const Button = styled.button`
+ background-color: #337ab7;
+ color: #fff;
+ font-size: 16px;
+ font-weight: bold;
+ padding: 10px;
+ cursor: pointer;
+`
+
+const App = () => {
+ return (
+  <Board>
+   <TopBar>
+    <TopBarTitle>게시판</TopBarTitle>
+   </TopBar>
+   <LeftColumn>
+    <Heading>왼쪽 열</Heading>
+    <Image src="https://picsum.photos/id/45/600/400" />
+    <Paragraph>왼쪽 열에 들어갈 내용</Paragraph>
+    <Button>추천</Button>
+   </LeftColumn>
+   <RightColumn>
+    <Heading>오른쪽 열</Heading>
+    <Image src="https://picsum.photos/id/46/600/400" />
+    <Paragraph>오른쪽 열에 들어갈 내용</Paragraph>
+    <Button>추천</Button>
+   </RightColumn>
+  </Board>
+ );
+};
 
 export default App;
